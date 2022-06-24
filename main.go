c1fb09744b91339b88441a41ece149868d643d9e
syfulsharif <sharif.syful@gmail.com>
2022-06-24 20:40:03 +0600

    Varialbes added

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-function main () {
-	Print("Hello World")
+import "fmt"
+
+func main() {
+
+	var conferenceName = "Go Conference"
+	fmt.Println("Welcome to", conferenceName, "booking application.")
+	fmt.Println("Get your tickets here to attend.")
+
 }
