05c7932dda9dd0a91cf9521fadf326ad24b0fbfb
syfulsharif <sharif.syful@gmail.com>
2022-06-24 20:47:57 +0600

    Varialbes and Constants updated

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
 	fmt.Println("Welcome to", conferenceName, "booking application.")
+	fmt.Println("We have total of", conferenceTickets, "tickets and currently we have", remainingTickets, "tickets available")
 	fmt.Println("Get your tickets here to attend.")
 
 }
